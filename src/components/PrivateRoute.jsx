cbe229c5d9906e26704f116c2981f60eba7eaf2d
eclipse7-9 <sanzspool@gmail.com>
2025-10-01 08:26:00 -0500

    main.jsx

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import {Navigate} from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
+
+
+
+export default function PrivateRoute({ children }) {
+    const { user } = useAuth()
+
+    return user ? children : <Navigate to="/login" />
+
+
+
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
+import Login from './pages/Login.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
+import { Navigate } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/app"
+          element={
+            <PrivateRoute>
+              <App />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+      <ToastContainer position='top-right' autoClose={3000} />
+    </AuthProvider>
+  </BrowserRouter>
+)
